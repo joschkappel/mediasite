a48587f44ab52554bab26697de576facc481dff5
jochen kappel <jkappel@onlinehome.de>
2023-02-14 19:01:28 +0100

    hamburger replaced

--- resources/views/livewire/gallery/gallery-info.blade.php
+++ resources/views/livewire/gallery/gallery-info.blade.php
@@ -1,5 +1,31 @@
 <div id="menu" class="row-span-10 p-2 overflow-auto h-screen">
-    <div id="menu" class="">
-        INFO
+    <div id="menu" class="hidden">
+        Lioba Kappel (b.1995)
+        I'm a portrait photographer based in Dortmund and Frankfurt, Germany
+
+        For job inquiries get in touch via email.
+
+        Contact
+        +49 17664701713
+        lioba.kappel@gmail.com
+        instagram
+
+
+
+        Education
+        Since 2022 MA Photographic Studies, University of Applied Science Dortmund
+        2022 BA Photography, University of Applied Science Dortmund
+
+
+        Selected (group) Exhibition
+        2022 NEXT! Festival der Jungen Photoszene, Museum für Angewandte Kunst, Köln (with Hauspoststille)
+        2022 Don‘t Dream It‘s Over, Künstlerhaus Dortmund
+        2020 „What come after?“- interaction Leipzig (with Hauspoststille)
+        2019 You:Ko, Bundeszentrale für politische Bildung, Bochum
+        2019 Identity. Courage. Love, Schadowplatz Düsseldorf
+
+        Publications
+        2022 put on your girl face, artist book, selfpublished
+        2019 juxtaposition, artist book, selfpublished
     </div>
 </div>
--- resources/views/livewire/gallery/main-nav.blade.php
+++ resources/views/livewire/gallery/main-nav.blade.php
@@ -1,7 +1,6 @@
 <header class="flex-shrink-0 flex items-center justify-between px-4 py-3 ">
     <div class="w-64 ">
         <p class="text-2xl font-bold tracking-wider">Lioba Kappel</p>
-        <p class="text-md font-bold">photographer</p>
     </div>
 
     <!-- Navigation Links -->
@@ -21,12 +20,14 @@
 
     <div x-data="{ open: false }" x-cloak @click.away="open = false" @keydown.escape.window="open = false">
         <button @click="open = !open" class="flex md:hidden items-center focus:outline-none">
+
             <svg class="h-6 w-6 text-gray-500" viewBox="0 0 24 24" fill="none">
-                <path x-show="!open" d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" stroke-width="2"
-                    stroke-linecap="round" stroke-linejoin="round" />
+                {{--                 <path x-show="!open" d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round" /> --}}
                 <path x-show="open" d="M6 18L18 6M6 6l12 12" stroke="currentColor" stroke-width="2"
                     stroke-linecap="round" stroke-linejoin="round" />
             </svg>
+            <div x-show="!open">menu</div>
         </button>
 
         <div x-show="open" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-lg z-10">
